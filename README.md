7f789171666e9bf279eb77a8c6bbf284f49f89fa
Subin Sebastian <subin@xtel.in>
2017-03-28 07:39:30 +0530

    Readme updated

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Bus
 ====
 
 A very light weight android event bus library. It does not provide many features, but simply lets you subscribe, unsubscribe and post events all within a few milliseconds. Customize if you want further features.
+
+```
+compile 'in.xtel.bus:bus:1.0.1'
+```
--- app/src/main/java/in/xtel/busandroid/MainActivity.java
+++ app/src/main/java/in/xtel/busandroid/MainActivity.java
@@ -1,9 +1,13 @@
 package in.xtel.busandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 import in.xtel.bus.Bus;
+import in.xtel.bus.Subscribe;
+import in.xtel.bus.ThreadMode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +21,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void onButtonClick(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Bus.getInstance().post(130);
+            }
+        }).start();
+    }
+
+    @Subscribe(ThreadMode.UI_THREAD)
+    public void onSomeEvent(int i) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setText("" + i);
+    }
+
+    @Override
+    protected void onPause() {
+        Bus.getInstance().unsubscribe(this);
+        super.onPause();
     }
 }
